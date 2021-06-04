003319b37b6f21110256d0008a0fbb8a88fdc90b
Peter <changhyeon@treenod.com>
2021-06-04 18:07:30 +0900

    종속성 주입 패턴 테스트 프로젝트 추가

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleDI.Example;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace DITestConsoleApp
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            ExemplifyScoping(host.Services, "Scope 1");
+            ExemplifyScoping(host.Services, "Scope 2");
+
+            return host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services) => {
+                    services.AddTransient<ITransientOperation, DefaultOperation>()
+                            .AddScoped<IScopedOperation, DefaultOperation>()
+                            .AddSingleton<ISingletonOperation, DefaultOperation>()
+                            .AddTransient<OperationLogger>();
+                });
+
+        static void ExemplifyScoping(IServiceProvider services, string scope)
+        {
+            using IServiceScope serviceScope = services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+
+            OperationLogger logger = provider.GetRequiredService<OperationLogger>();
+            logger.LogOperations($"{scope}-Call 1 .GetRequiredService<OperationLogger>()");
+
+            Console.WriteLine("...");
+
+            logger = provider.GetRequiredService<OperationLogger>();
+            logger.LogOperations($"{scope}-Call 2 .GetRequiredService<OperationLogger>()");
+
+            Console.WriteLine();
+        }
+    }
+}
